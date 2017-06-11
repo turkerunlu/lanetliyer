c5479c34fb245d896f9d75451899012b7bf47207
tunahamza <hamzatuna74@gmail.com>
2017-06-11 14:11:33 +0300

    delete

--- faruk.py
+++
@@ -1,8 +0,0 @@
-kardes1=int(raw_input('birinci kardesin yasini giriniz: '))
-kardes2=int(raw_input('ikinci kardesin yasini giriniz: '))
-fark=kardes1-kardes2
-toplam=kardes1+kardes2
-if fark<0:
-	fark=fark*-1
-print 'farklari=%d dir'%fark
-print 'toplamlari=%d dir'%toplam
